fdc87f16ab5a0491aaac0a903eb1f5477f1db5c9
altermanus <63774806+altermanus@users.noreply.github.com>
2026-09-05 09:59:51 +0200

    Create timelimit.js

---
+++ timelimit.js
@@ -0,0 +1,14 @@
+// Plugin configuration, this is used in the administration when plugins are loaded
+var pluginConfig = {
+    name: 'Time Limit',
+    version: 'AI-1v',
+    author: 'altermanus',
+    frontEndPath: 'timelimit/TimeLimit.js'
+}
+
+// Backend (server) changes can go here...
+
+// Don't change anything below here if you are making your own plugin
+module.exports = {
+    pluginConfig
+}
